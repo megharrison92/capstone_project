0705b5a9e3a9b41e58661c5eae8994ebca5a4822
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-10-12 13:53:23 -0500

    register formik

--- client/src/RegisterForm.js
+++ client/src/RegisterForm.js
@@ -1,6 +1,26 @@
-
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 function RegisterForm() {
+    
+    const RegisterFormSchema = yup.object().shape({
+        firstname: yup.string().required('Please enter a firstname'),
+        lastname: yup.string().required('Please enter a lastname'),
+        username: yup.string().required('Please enter a username'),
+        password: yup.string().required('Please enter a password'),
+    })
+    
+    const{ values, handleBlur, handleChange, handleSubmit}  = useFormik ({
+        initialValues: {
+            firstname: '',
+            lastname: '',
+            username: '',
+            password: '',
+        },
+        validationSchema: RegisterFormSchema
+    })
+    
     return (
         <div>
             <h2>Register</h2>
